ec825828c6debc971461d4c4eebfe737d6d5da9a
Jemmie Renard <jemmierenard17@gmail.com>
2021-12-13 08:12:44 +0700

    Add project files.

---
+++ Praktikum W14/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktikum_W14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input : ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Output : ");
+			int jumlah = 0;
+			for (int i = 1; i <= input; i++)
+			{
+				int sisa = input % i;
+				if (sisa == 0)
+				{
+					jumlah = jumlah + 1;
+				}
+			}
+
+			if (jumlah > 2)
+			{
+				Console.WriteLine("Bukan angka prima");
+			}
+			else
+			{
+				Console.WriteLine("Angka prima");
+			}
+		}
+    }
+}
